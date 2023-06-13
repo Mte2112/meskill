a0b67521c685db9b31270fbae5c8907649ba5d9f
MaxElling <70956803+Mte2112@users.noreply.github.com>
2023-06-13 11:58:31 -0400

    Delete meskill directory

--- meskill/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='meskill',
-      version='1.0',
-      description='Skill scorer for ModelE diagnostic output. Inspired by GISS ModelE RMS program.',
-      packages=['scorer'],
-      authors_contact='maxwell.t.elling@nasa.gov; sanketa.s.kadam@nasa.gov',
-      zip_safe=False)
